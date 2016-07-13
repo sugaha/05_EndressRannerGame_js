a4196276acee4d710908357770975b7c86710bbf
sugaha <gorba1442@gmail.com>
2016-07-13 12:02:29 +0900

    first commit (add files)

--- src/resource.js
+++ src/resource.js
@@ -3,7 +3,9 @@ var res = {
     background_png : "res/background.png",
     ship_png : "res/ship.png",
     particle_png : "res/particle.png",
-    asteroid_png : "res/asteroid.png"
+    asteroid_png : "res/asteroid.png",
+    bgm_main : "res/bgm_main.mp3",
+    se_bang : "res/se_bang.mp3"
 };
 
 var g_resources = [];
